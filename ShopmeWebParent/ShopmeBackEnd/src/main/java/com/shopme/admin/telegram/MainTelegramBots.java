7c27a001501af81f3892468fe9f8b848c3d57312
cchannawebdeveloper <cchanna.y.oeurng@gmail.com>
2022-07-20 18:24:44 +0700

    remove telegram bot

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/telegram/MainTelegramBots.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/telegram/MainTelegramBots.java
@@ -1,29 +1,29 @@
-package com.shopme.admin.telegram;
-
-import org.telegram.telegrambots.ApiContextInitializer;
-import org.telegram.telegrambots.TelegramBotsApi;
-import org.telegram.telegrambots.exceptions.TelegramApiException;
-
-public class MainTelegramBots {
-
-	public static void main(String[] args) {
-		 
-		// TODO Initialize Api Context
-
-        ApiContextInitializer.init();
-
-        // TODO Instantiate Telegram Bots API
-
-        TelegramBotsApi botsApi = new TelegramBotsApi();
-
-        // TODO Register our bot
-
-        try {
-            botsApi.registerBot(new TelegramBots());
-        } catch (TelegramApiException e) {
-           // e.printStackTrace();
-        }
-
-	}
-
-}
+//package com.shopme.admin.telegram;
+//
+//import org.telegram.telegrambots.ApiContextInitializer;
+//import org.telegram.telegrambots.TelegramBotsApi;
+//import org.telegram.telegrambots.exceptions.TelegramApiException;
+//
+//public class MainTelegramBots {
+//
+//	public static void main(String[] args) {
+//		 
+//		// TODO Initialize Api Context
+//
+//        ApiContextInitializer.init();
+//
+//        // TODO Instantiate Telegram Bots API
+//
+//        TelegramBotsApi botsApi = new TelegramBotsApi();
+//
+//        // TODO Register our bot
+//
+//        try {
+//            botsApi.registerBot(new TelegramBots());
+//        } catch (TelegramApiException e) {
+//           // e.printStackTrace();
+//        }
+//
+//	}
+//
+//}
--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/telegram/TelegramBots.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/telegram/TelegramBots.java
@@ -1,42 +1,42 @@
-package com.shopme.admin.telegram;
-
-import org.telegram.telegrambots.api.methods.send.SendMessage;
-import org.telegram.telegrambots.api.objects.Update;
-import org.telegram.telegrambots.bots.TelegramLongPollingBot;
-import org.telegram.telegrambots.exceptions.TelegramApiException;
-
-public class TelegramBots extends TelegramLongPollingBot {
-
-	@Override
-	public void onUpdateReceived(Update update) {
-		// TODO Auto-generated method stub
-		
-		 System.out.println("Message chat word!!");
-         String message_text = update.getMessage().getText();
-         long chat_id = update.getMessage().getChatId();
-
-         SendMessage message = new SendMessage();
-        message.setChatId(chat_id);
-        message.setText(message_text);
-
-         try {
-             execute(message);
-         } catch (TelegramApiException ex) {
-           //  ex.printStackTrace();
-         }
-		
-	}
-
-	@Override
-	public String getBotUsername() {
-		// TODO Auto-generated method stub
-		return "@ibtesting_bot";
-	}
-
-	@Override
-	public String getBotToken() {
-		// TODO Auto-generated method stub
-		return "5566568060:AAE6CzVDsxGBIZqKwaEBGlPHAzh4saur1Lw";
-	}
-
-}
+//package com.shopme.admin.telegram;
+//
+//import org.telegram.telegrambots.api.methods.send.SendMessage;
+//import org.telegram.telegrambots.api.objects.Update;
+//import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+//import org.telegram.telegrambots.exceptions.TelegramApiException;
+//
+//public class TelegramBots extends TelegramLongPollingBot {
+//
+//	@Override
+//	public void onUpdateReceived(Update update) {
+//		// TODO Auto-generated method stub
+//		
+//		 System.out.println("Message chat word!!");
+//         String message_text = update.getMessage().getText();
+//         long chat_id = update.getMessage().getChatId();
+//
+//         SendMessage message = new SendMessage();
+//        message.setChatId(chat_id);
+//        message.setText(message_text);
+//
+//         try {
+//             execute(message);
+//         } catch (TelegramApiException ex) {
+//           //  ex.printStackTrace();
+//         }
+//		
+//	}
+//
+//	@Override
+//	public String getBotUsername() {
+//		// TODO Auto-generated method stub
+//		return "@ibtesting_bot";
+//	}
+//
+//	@Override
+//	public String getBotToken() {
+//		// TODO Auto-generated method stub
+//		return "5566568060:AAE6CzVDsxGBIZqKwaEBGlPHAzh4saur1Lw";
+//	}
+//
+//}
